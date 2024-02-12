3d36f7d13265a977bf4b0fd3e9c8f80414ad4f04
hexaalif <shadullahsad9@gmail.com>
2024-02-12 13:49:39 +0600

    patient model created

--- patient/admin.py
+++ patient/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Patient
+from . import models
 # Register your models here.
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['first_name','mobile_no']
+
+    def first_name(self, obj):
+        return obj.user.first_name
+
+admin.site.register(Patient, PatientAdmin)
