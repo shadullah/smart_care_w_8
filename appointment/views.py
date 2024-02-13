5fa3cc782e5187f001bc619d2f56d32cb10ead7d
hexaalif <shadullahsad9@gmail.com>
2024-02-14 00:51:13 +0600

    patient model fixed

--- appointment/views.py
+++ appointment/views.py
@@ -7,3 +7,11 @@ from . import serializers
 class AppointmentViewSet(viewsets.ModelViewSet):
     queryset = models.Appointment.objects.all()
     serializer_class = serializers.AppointmentSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        print(self.request.query_params)
+        patient_id = self.request.query_params.get('patient_id')
+        if patient_id:
+            queryset = queryset.filter(patient_id=patient_id)
+        return queryset
