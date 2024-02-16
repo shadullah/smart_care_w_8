d52b50a499f8b2afdf23ec006da1c4427f816451
hexaalif <shadullahsad9@gmail.com>
2024-02-16 18:32:11 +0600

    deploy requirements

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-nq89$*-&47+21dw@+$o3^s3vm=1_!zb#be@69o84=u2888&^02
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
