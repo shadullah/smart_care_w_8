7f16b0e27a511e19bebd93b25661a0314eee4366
hexaalif <shadullahsad9@gmail.com>
2024-02-16 14:43:13 +0600

    patient registration view created

--- patient/views.py
+++ patient/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from . import models
 from . import serializers
 from rest_framework.views import APIView
+from rest_framework.response import Response
+
 # Create your views here.
 class PatientViewset(viewsets.ModelViewSet):
     queryset = models.Patient.objects.all()
@@ -10,3 +12,12 @@ class PatientViewset(viewsets.ModelViewSet):
 
 class UserRegistrationApiView(APIView):
     serializer_class = serializers.RegistrationSerializer
+
+    def post(self, req):
+        serializer = self.serializer_class(data=req.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            print(user)
+            return Response("form done")
+        return Response(serializer.errors)
